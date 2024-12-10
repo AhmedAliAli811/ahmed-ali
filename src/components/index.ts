dab1c767f35dad96cd100bb75bf61967ecd3ffbc
Ahmed Ali Ali <81569432+AhmedAliAli811@users.noreply.github.com>
2024-12-10 18:08:13 +0200

    delete

--- src/components/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as Grid } from "@/components/Grid";
-export { default as Header } from "@/components/Header";
-export { default as Footer } from "@/components/Footer";
-export { default as Column } from "@/components/Column";
-export { default as Section } from "@/components/Section";
-export { default as TimeLine } from "@/components/TimeLine";
-export { default as Container } from "@/components/Container";
-export { default as Nav } from "@/components/Nav";
