40086953424a87dbab5ab06065d04ca9d04801be
Ahmed Ali Ali <81569432+AhmedAliAli811@users.noreply.github.com>
2024-12-11 00:17:29 +0200

    last dance

--- src/app/hero/page.tsx
+++ src/app/hero/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-"use client";
-
 import api from "@/data/api";
 import Link from "next/link";
 import { Column, Section } from "@/components";
@@ -9,38 +7,41 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <Section id="about">
-      <Column style="md-10 gap-3">
-        <h1 className="text-4xl md:text-4xl font-bold">
-          <span className="text-[#f2f2f2]">{api.hero.title}</span>
+    <Section id="about" style="py-8 mt-12">
+      <Column style="gap-6 text-center md:text-left">
+        {/* Title */}
+        <h1 className="text-3xl md:text-4xl font-bold">
+          <span className="text-[#f2f2f2]">{api.hero.title}</span>{" "}
           <span className="text-[#ccad00]">{api.hero.name}</span>
         </h1>
-        <p className="text-xs md:text-2xl">{api.hero.description}</p>
 
-        <div className="flex items-center gap-4 mt-4">
-          {/* CV Link */}
+        {/* Description */}
+        <p className="text-sm md:text-xl">{api.hero.description}</p>
+
+        {/* CV and Social Links */}
+        <div className="flex flex-col md:flex-row items-center md:items-start gap-4 mt-6">
+          {/* CV Button */}
           <Link
             href={api.hero.cv.link}
             target="_blank"
-            className="w-12 h-12 text-black font-bold bg-[#f2f2f2] border rounded-lg flex items-center justify-center gap-1"
+            className="w-32 h-12 hover:text-[#f2f2f2] hover:bg-[#998200] text-black font-bold bg-[#f2f2f2] border rounded-lg flex items-center justify-center gap-2 transition"
           >
             {api.hero.cv.title}
-            <i className="fa-regular fa-file"></i> {/* Icon for CV */}
           </Link>
 
           {/* Social Icons */}
-          <div className="flex gap-4 ">
+          <div className="flex flex-wrap gap-4 justify-center md:justify-start">
             {api.hero.social.map((social, index) => (
               <Link
                 key={index}
                 href={social.link}
                 target="_blank"
-                className="text-2xl w-12 h-12 text-black font-bold bg-[#f2f2f2] border rounded-lg flex items-center justify-center gap-1  "
+                className="w-12 h-12 hover:bg-[#998200] text-black font-bold bg-[#f2f2f2] border rounded-lg flex items-center justify-center transition"
               >
                 <Image
-                    src={social.img}
-                    className="w-8 h-8"
-                    alt={social.title}
+                  src={social.img}
+                  className="w-6 h-6 md:w-8 md:h-8"
+                  alt={social.title}
                 />
               </Link>
             ))}
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,131 +1,66 @@
-// // components/NavBar.tsx
-// import React from 'react';
-// import Link from 'next/link';
-//
-// const Nav: React.FC = () => {
-//     return (
-//         <nav className="flex text-white items-center justify-between bg-gray-800 p-4">
-//             <div className="text-2xl font-bold bokor-regular">
-//                 <Link href="/">
-//                     Ahmed Ali Ali
-//                 </Link>
-//             </div>
-//             <ul className="flex space-x-4">
-//                 <li>
-//                     <Link href="/#about">
-//                         About Me
-//                     </Link>
-//                 </li>
-//                 <li>
-//                     <Link href="/#exper">
-//                         Experience
-//                     </Link>
-//                 </li>
-//                 <li>
-//                     <Link href="/#projects">
-//                         Projects
-//                     </Link>
-//                 </li>
-//                 <li>
-//                     <Link href="/#skills">
-//                         Skills
-//                     </Link>
-//                 </li>
-//                 <li>
-//                     <Link href="/#contact">
-//                         Contact Me
-//                     </Link>
-//                 </li>
-//             </ul>
-//         </nav>
-//     );
-// };
-//
-// export default Nav;
-//
-// import React from 'react';
-// import { Navbar, Container, Nav } from 'react-bootstrap';
-// import Link from 'next/link';
-//
-// const NavBar = () => {
-//   return (
-//     <Navbar bg="dark" variant="dark" expand="lg">
-//       <Container>
-//         <Navbar.Brand href="/">
-//             Ahmed Ali Ali
-//         </Navbar.Brand>
-//
-//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//         <Navbar.Collapse id="basic-navbar-nav">
-//           <Nav className="ms-auto">
-//             <Nav.Link href="#about">About Me</Nav.Link>
-//             <Nav.Link href="#exper">Experience</Nav.Link>
-//             <Nav.Link href="#projects">Projects</Nav.Link>
-//             <Nav.Link href="#skills">Skills</Nav.Link>
-//             <Nav.Link href="#contact">Contact Me</Nav.Link>
-//           </Nav>
-//         </Navbar.Collapse>
-//       </Container>
-//     </Navbar>
-//   );
-// };
-//
-// export default NavBar;
-// import React from "react";
-// import Link from "next/link";
-//
-// const NavBar = () => {
-//   return (
-//     <nav className="bg-gray-800 text-white py-4">
-//       <div className="  mx-auto flex items-center justify-between px-6">
-//         <Link href="/" className="text-2xl font-semibold">
-//           Ahmed Ali Ali
-//         </Link>
-//         <div className="flex space-x-6">
-//           <Link href="#about" className="hover:text-gray-400">About Me</Link>
-//           <Link href="#exper" className="hover:text-gray-400">Experience</Link>
-//           <Link href="#projects" className="hover:text-gray-400">Projects</Link>
-//           <Link href="#skills" className="hover:text-gray-400">Skills</Link>
-//           <Link href="#contact" className="hover:text-gray-400">Contact Me</Link>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
-//
-// export default NavBar;
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import Link from "next/link";
 
 const NavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <nav className="bg-gray-800 text-white py-4 font-bold">
+    <nav className="fixed top-0 left-0 right-0 z-50 text-white py-4 font-bold shadow-lg">
       <div className="mx-auto flex items-center justify-between px-6">
+        {/* Logo */}
         <Link href="/" className="text-xl font-bold font-bokor">
           Ahmed Ali
         </Link>
-        <div className="flex space-x-6">
+
+        {/* Hamburger Menu (Visible on Small Screens) */}
+        <button
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          className="text-white focus:outline-none md:hidden"
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d={isMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+            />
+          </svg>
+        </button>
+
+        {/* Menu Links */}
+        <div
+          className={`${
+            isMenuOpen ? "block" : "hidden"
+          } absolute top-16 left-0 w-full bg-gray-800 md:static md:flex md:w-auto md:space-x-6 md:bg-transparent`}
+        >
           <Link
             href="/#about"
-            className="hover:bg-[#ccad00] hover:text-gray-900 px-4 py-2 rounded-lg transition duration-300"
+            className="block px-4 py-2 hover:bg-[#ccad00] hover:text-gray-900 rounded-lg transition duration-300 md:inline"
           >
             About Me
           </Link>
           <Link
             href="/#exper"
-            className="hover:bg-[#ccad00] hover:text-gray-900 px-4 py-2 rounded-lg transition duration-300"
+            className="block px-4 py-2 hover:bg-[#ccad00] hover:text-gray-900 rounded-lg transition duration-300 md:inline"
           >
             Experience
           </Link>
           <Link
             href="/#projects"
-            className="hover:bg-[#ccad00] hover:text-gray-900 px-4 py-2 rounded-lg transition duration-300"
+            className="block px-4 py-2 hover:bg-[#ccad00] hover:text-gray-900 rounded-lg transition duration-300 md:inline"
           >
             Projects
           </Link>
           <Link
             href="/#skills"
-            className="hover:bg-[#ccad00] hover:text-gray-900 px-4 py-2 rounded-lg transition duration-300"
+            className="block px-4 py-2 hover:bg-[#ccad00] hover:text-gray-900 rounded-lg transition duration-300 md:inline"
           >
             Skills
           </Link>
@@ -136,4 +71,3 @@ const NavBar = () => {
 };
 
 export default NavBar;
-
