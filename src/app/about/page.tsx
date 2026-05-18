660f04e9f811f918f3f22c63c4a9d5df47b23146
Ahmed Ali Ali <81569432+AhmedAliAli811@users.noreply.github.com>
2026-05-18 19:53:45 +0300

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Column, Header, Section } from "@/components";
-
+import api from "@/data/api";
 const About = () => {
   return (
     <Section id="about" style="py-8">
@@ -13,13 +13,7 @@ const About = () => {
 
         {/* Description */}
         <p className="text-sm md:text-xl w-full md:w-2/3 mt-4 text-center md:text-left leading-relaxed">
-          I am a fresh graduate from the Faculty of Computers and Artificial Intelligence at Cairo University,
-          specializing in Artificial Intelligence. Passionate about data analysis and business intelligence, I excel
-          at transforming raw data into actionable insights. Proficient in SQL, Python, and advanced visualization
-          tools like Power BI, I have a strong foundation in statistics, machine learning, and database management. I
-          enjoy solving real-world problems by uncovering trends, optimizing processes, and delivering impactful
-          solutions. Dedicated to continuous learning and innovation, I am driven to craft meaningful narratives that
-          drive informed decisions and business success.
+          {api.about.description}
         </p>
       </Column>
     </Section>
